54ab809eb3846b9c5d1b58eecb3bfe82cb0566d9
marcelo <marceloestudoifes@gmail.com>
2019-07-03 15:38:29 -0300

    novo_projeto

--- projeto/ServeletAutopecas/src/java/LN/Funcionario.java
+++
@@ -1,9 +0,0 @@
-package LN;
-
-public class Funcionario extends Pessoa{
-
-    public Funcionario(Data nascimento, String nomeCompleto, Cpf cpf, Usuario user) {
-        super(nascimento, nomeCompleto, cpf, user);
-    }
-    
-}
